cb63a8dced6ab1415c08ba5e70d5d029b5eb3e2d
Smart Lazarus <smartlazarus@gmail.com>
2023-08-15 04:28:16 -0700

    AirBNB clone - console

--- models/user.py
+++ models/user.py
@@ -7,7 +7,14 @@ import json
 
 
 class User(BaseModel):
-    '''base modell class'''
+    '''base model class
+
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    '''
 
     email = ""
     password = ""
